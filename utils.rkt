e0947f86974eb3130099e38038353434b6649952
oliviaq <queathem1@grinnell.edu>
2014-04-02 21:12:47 -0500

    Added summary.

--- utils.rkt
+++ utils.rkt
@@ -1,5 +1,11 @@
 #lang racket
 (require gigls/unsafe)
+;;; File:
+;;;   utils.rkt
+;;; Author:
+;;;   Olivia Queathem
+;;; Summary:
+;;;   A few sample utilities, created as part of a lab on GitHub.
 ;;; Procedure:
 ;;;   nums
 ;;; Parameters:
